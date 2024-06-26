c2ddd2fc894504daf14bea7398e88b591c6b9d24
thinnerthinker <thinnrthinkr@gmail.com>
2024-06-26 18:11:39 +0300

    try

--- sursface/src/time.rs
+++ sursface/src/time.rs
@@ -1,39 +1,111 @@
-use lazy_static::lazy_static;
+#![allow(dead_code, unused_imports)]
 
-#[cfg(target_arch = "wasm32")]
-mod platform_time {
-    use wasm_bindgen::prelude::*;
-    use wasm_bindgen::JsCast;
+use wasm_bindgen::prelude::*;
+use std::convert::TryInto;
+use std::ops::{Add, Sub, AddAssign, SubAssign};
 
-    pub fn current_time() -> f64 {
-        web_sys::window()
-            .expect("no global `window` exists")
-            .performance()
-            .expect("performance should be available")
-            .now() / 1000.0 // Convert milliseconds to seconds
-    }
-}
+pub use std::time::*;
 
 #[cfg(not(target_arch = "wasm32"))]
-mod platform_time {
-    use std::time::Instant;
-    use lazy_static::lazy_static;
+#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct Instant(std::time::Instant);
 
-    lazy_static! {
-        static ref START: Instant = Instant::now();
+#[cfg(not(target_arch = "wasm32"))]
+impl Instant {
+    pub fn now() -> Self {
+        Self(std::time::Instant::now())
     }
-
-    pub fn current_time() -> f64 {
-        START.elapsed().as_secs_f64()
+    pub fn duration_since(&self, earlier: Instant) -> Duration {
+        self.0.duration_since(earlier.0)
+    }
+    pub fn elapsed(&self) -> Duration {
+        self.0.elapsed()
+    }
+    pub fn checked_add(&self, duration: Duration) -> Option<Self> {
+        self.0.checked_add(duration).map(Self)
+    }
+    pub fn checked_sub(&self, duration: Duration) -> Option<Self> {
+        self.0.checked_sub(duration).map(Self)
     }
 }
 
+#[cfg(target_arch = "wasm32")]
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(js_namespace = Date, js_name = now)]
+    fn date_now() -> f64;
+}
+
+#[cfg(target_arch = "wasm32")]
+#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct Instant(u64);
+
+#[cfg(target_arch = "wasm32")]
+impl Instant {
+    pub fn now() -> Self {
+        Self((date_now() * 1_000.0) as u64) // Convert seconds to milliseconds
+    }
+    pub fn duration_since(&self, earlier: Instant) -> Duration {
+        Duration::from_millis(self.0 - earlier.0)
+    }
+    pub fn elapsed(&self) -> Duration {
+        Self::now().duration_since(*self)
+    }
+    pub fn checked_add(&self, duration: Duration) -> Option<Self> {
+        match duration.as_millis().try_into() {
+            Ok(duration) => self.0.checked_add(duration).map(Self),
+            Err(_) => None,
+        }
+    }
+    pub fn checked_sub(&self, duration: Duration) -> Option<Self> {
+        match duration.as_millis().try_into() {
+            Ok(duration) => self.0.checked_sub(duration).map(Self),
+            Err(_) => None,
+        }
+    }
+}
+
+impl Add<Duration> for Instant {
+    type Output = Instant;
+    fn add(self, other: Duration) -> Instant {
+        self.checked_add(other).unwrap()
+    }
+}
+
+impl Sub<Duration> for Instant {
+    type Output = Instant;
+    fn sub(self, other: Duration) -> Instant {
+        self.checked_sub(other).unwrap()
+    }
+}
+
+impl Sub<Instant> for Instant {
+    type Output = Duration;
+    fn sub(self, other: Instant) -> Duration {
+        self.duration_since(other)
+    }
+}
+
+impl AddAssign<Duration> for Instant {
+    fn add_assign(&mut self, other: Duration) {
+        *self = *self + other;
+    }
+}
+
+impl SubAssign<Duration> for Instant {
+    fn sub_assign(&mut self, other: Duration) {
+        *self = *self - other;
+    }
+}
+
+use lazy_static::lazy_static;
+
 lazy_static! {
-    static ref START_TIME: f64 = platform_time::current_time();
+    static ref START_TIME: Instant = Instant::now();
 }
 
 pub fn now() -> f32 {
-    (platform_time::current_time() - *START_TIME) as f32
+    Instant::now().duration_since(*START_TIME).as_secs_f32()
 }
 
 fn main() {
