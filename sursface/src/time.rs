3ecad6b7fab303c2909e5563d154908468372e20
thinnerthinker <thinnrthinkr@gmail.com>
2024-06-26 19:05:38 +0300

    try

--- sursface/src/time.rs
+++ sursface/src/time.rs
@@ -33,6 +33,6 @@ pub fn now_secs() -> f32 {
         let performance = window.performance().unwrap();
             
         let elapsed = performance.now() - *START_TIME;
-        (elapsed / 1000.0) as f32 * 1000f32 // Convert milliseconds to seconds
+        (elapsed) as f32
     }
 }
